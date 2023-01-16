07370792cac129b82b2e0b17004b62590dd64eaa
jtbandurski <jakub.t.bandurski@gmail.com>
2023-01-16 18:31:42 +0100

    still problems with getting data from the form

--- project/habitstracker/habits/urls.py
+++ project/habitstracker/habits/urls.py
@@ -7,5 +7,6 @@ app_name = 'habits'
 urlpatterns = [
     path('',views.index, name='index'),
     path('login/',views.login, name='login'),
-    path('add_progress/',views.add_progress, name='add_progress')
+    path('add_progress/',views.add_progress, name='add_progress'),
+    path('add_progress/submission/',views.add_progress_submission,name='add_progress_submission'),
 ]
--- project/habitstracker/habits/views.py
+++ project/habitstracker/habits/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Posts, Habbits
+
 
 # Create view
 # Get and display Posts
@@ -20,3 +21,19 @@ def add_progress(request):
     habits_list = Habbits.objects.all().filter(user=1)
     context = {'habits': habits_list}
     return render(request, 'habits/add_progress.html', context)
+
+def add_progress_submission(request):
+    # ADD AUTHORIZATION AND USER DETAILS
+    if request.method == "POST":
+        
+        habit_id = request.POST["selected_habit"]
+        amount = request.POST["amount"]
+        date = request.POST["date"]
+        add = HabbitsTracker(habbit = habit_id,habbit_type=1,quantity=quantity,date = date,yes_no_value= None, success_amount_value=amount)
+        add.save()
+
+        posts_lists = Posts.objects.order_by('-publish_date')[:10]
+        context = {'posts': posts_lists}
+
+        return render(request,'habits/index.html',context)
+    return redirect('/habits/add_progress')
