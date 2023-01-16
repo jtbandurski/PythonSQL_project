1a2a9fa66f3ba123258347593db8f16eafaadb32
jtbandurski <jakub.t.bandurski@gmail.com>
2023-01-17 00:24:03 +0100

    Revert "mergee"
    This reverts commit 81577247ce6637b1ffc30ed80e46cda61495d874, reversing
    changes made to 67f5f3d57753fed555d686d0a42a3566c40c2352.

--- project/habitstracker/habits/urls.py
+++ project/habitstracker/habits/urls.py
@@ -7,8 +7,7 @@ from . import views
 urlpatterns = [
     path('',views.index, name='index'),
     path('login/',views.login, name='login'),
-    path('register/',views.register, name='register'),
-    path('logout/',views.logout, name='logout'),
+    path('logout/',views.login, name='logout'),
     path('add_progress/',views.add_progress, name='add_progress'),
     path('add_progress/submission/',views.add_progress_submission,name='add_progress_submission'),
     path('my_habits/',views.my_habits, name='my_habits'),
